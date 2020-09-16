c68824195026ab9eac57376e5d438bab9758a114
Christerle Nounoute <christerlesn@gmail.com>
2020-09-16 19:16:55 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,8 +18,7 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-shipping_manifest["muskets"] = 2
-shipping_manifest["gun powder"] = 4
+shipping_manifest["oil paintings"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
