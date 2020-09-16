0dd28a5f5a9cacb9166b379687ebbbd098553cc4
Christerle Nounoute <christerlesn@gmail.com>
2020-09-16 19:11:10 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+my_hash = {"grocery list"=> "peanuts, apple, juice", "phrarmacy list" => "ibeprofen, cough drops", "other errands"=> "wash car, fold clothes"}
   
 end
 
